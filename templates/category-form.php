3983edfd51eaca72a04fe13d1207bddab5530ec2
ryuheidesuyo1013 <ryuheidesuyo1013@gmail.com>
2026-07-31 16:24:50 +0900

    管理画面フォームのデザインを改善

--- templates/category-form.php
+++ templates/category-form.php
@@ -13,46 +13,87 @@ if (
 }
 ?>
 
-<form method="post" action="<?= escape($formAction) ?>">
+<form
+    method="post"
+    action="<?= escape($formAction) ?>"
+>
     <input
         type="hidden"
         name="csrf_token"
         value="<?= escape(getCsrfToken()) ?>"
     >
 
-    <div>
-        <label for="name">カテゴリ名</label>
+    <div class="form-group">
+        <label
+            class="form-label"
+            for="name"
+        >
+            カテゴリ名
+
+            <span class="form-required">
+                必須
+            </span>
+        </label>
 
         <input
+            class="form-control"
             type="text"
             id="name"
             name="name"
             value="<?= escape($category['name'] ?? '') ?>"
             maxlength="100"
+            autocomplete="off"
             required
         >
+
+        <p class="form-help">
+            記事の分類として管理画面や公開ページに表示される名称です。
+        </p>
     </div>
 
-    <div>
-        <label for="slug">スラッグ</label>
+    <div class="form-group">
+        <label
+            class="form-label"
+            for="slug"
+        >
+            スラッグ
+
+            <span class="form-required">
+                必須
+            </span>
+        </label>
 
         <input
+            class="form-control"
             type="text"
             id="slug"
             name="slug"
             value="<?= escape($category['slug'] ?? '') ?>"
             maxlength="100"
             pattern="[a-z0-9]+(?:-[a-z0-9]+)*"
+            autocomplete="off"
             required
         >
 
-        <p>
+        <p class="form-help">
             半角英小文字・数字・ハイフンで入力してください。
             例：physical-training
         </p>
     </div>
 
-    <button type="submit">
-        <?= escape($submitLabel) ?>
-    </button>
+    <div class="form-actions">
+        <button
+            class="button"
+            type="submit"
+        >
+            <?= escape($submitLabel) ?>
+        </button>
+
+        <a
+            class="button button--outline"
+            href="category-list.php"
+        >
+            キャンセル
+        </a>
+    </div>
 </form>
